64e07c756b2c5d88513fb07d65976a90b5ad687d
Filip <filipgolyszny@gmail.com>
2024-03-06 18:13:34 +0100

    remove comment

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 
-from main_window import MainWindow
-
-# set_wallpaper("C:/Users/filip/Pictures/wallpaper/samurai.png")
-
+from src.main_window import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
